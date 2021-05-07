27bdb409ec7727f021f739abf50bbb9eb6c26e85
Boris Fersing <boris@fersing.net>
2021-05-07 13:51:06 -0400

    Use a IProxyListener instead of a IHttpListener

--- burp/src/burp/BurpExtender.kt
+++ burp/src/burp/BurpExtender.kt
@@ -1,8 +1,9 @@
 package  burp
 
 import java.io.PrintWriter
+import java.util.*
 
-class BurpExtender : IBurpExtender, IHttpListener, IExtensionStateListener {
+class BurpExtender : IBurpExtender, IProxyListener, IExtensionStateListener {
 
     private lateinit var callbacks: IBurpExtenderCallbacks
     private lateinit var helpers: IExtensionHelpers
@@ -16,32 +17,34 @@ class BurpExtender : IBurpExtender, IHttpListener, IExtensionStateListener {
         this.stderr = PrintWriter(callbacks.stderr, true)
 
         callbacks.setExtensionName("PwnFox")
-        callbacks.registerHttpListener(this)
         callbacks.registerExtensionStateListener(this)
+        callbacks.registerProxyListener(this)
         stdout.println("PwnFox Loaded")
     }
 
 
-    override fun processHttpMessage(toolFlag: Int, messageIsRequest: Boolean, messageInfo: IHttpRequestResponse) {
-        if (!messageIsRequest) return
-        val requestInfo = helpers.analyzeRequest(messageInfo)
-        val body = messageInfo.request.drop(requestInfo.bodyOffset).toByteArray()
-        val (pwnFoxHeaders, cleanHeaders) = requestInfo.headers.partition {
-            it.toLowerCase().startsWith("x-pwnfox-")
-        }
-
-        pwnFoxHeaders.forEach() {
-            if (it.toLowerCase().startsWith(("x-pwnfox-color:"))) {
-                val (_, color) = it.split(":", limit = 2)
-                messageInfo.highlight = color.trim()
-            }
-        }
-
-        messageInfo.request =
-            helpers.buildHttpMessage(cleanHeaders, body)
-
+    override fun extensionUnloaded() {
     }
 
-    override fun extensionUnloaded() {
+    override fun processProxyMessage(messageIsRequest: Boolean, message: IInterceptedProxyMessage?) {
+        if (!messageIsRequest) return
+
+        val messageInfo = message?.messageInfo
+        if (messageInfo != null) {
+
+            val requestInfo = helpers.analyzeRequest(messageInfo)
+            val body = messageInfo.request.drop(requestInfo.bodyOffset).toByteArray()
+            val (pwnFoxHeaders, cleanHeaders) = requestInfo.headers.partition {
+                it.lowercase(Locale.getDefault()).startsWith("x-pwnfox-")
+            }
+
+            pwnFoxHeaders.forEach() {
+                if (it.lowercase(Locale.getDefault()).startsWith(("x-pwnfox-color:"))) {
+                    val (_, color) = it.split(":", limit = 2)
+                    messageInfo.highlight = color.trim()
+                }
+            }
+            messageInfo.request = helpers.buildHttpMessage(cleanHeaders, body)
+        }
     }
 }
